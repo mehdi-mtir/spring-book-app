f3e6b265174aa899bbbe3a7eba68721f6de9d687
MEHDI MTIR <mehdi.mtir@gmail.com>
2024-05-14 10:15:20 +0100

    First commit

--- booksapp/src/main/java/com/bna/booksapp/respositories/IBookRepository.java
+++ booksapp/src/main/java/com/bna/booksapp/respositories/IBookRepository.java
@@ -6,4 +6,12 @@ import java.util.List;
 
 public interface IBookRepository {
     public List<BookDto> findAll();
+
+    public  BookDto findById(long id);
+
+    public  BookDto create(BookDto book);
+
+    public BookDto update(BookDto book);
+
+    public BookDto delete(long id);
 }
--- booksapp/src/main/java/com/bna/booksapp/services/BookService.java
+++ booksapp/src/main/java/com/bna/booksapp/services/BookService.java
@@ -1,21 +1,57 @@
 package com.bna.booksapp.services;
 
 import com.bna.booksapp.dtos.BookDto;
-import com.bna.booksapp.respositories.IBookRepository;
+import com.bna.booksapp.entities.Book;
+import com.bna.booksapp.mappers.BookMapper;
+import com.bna.booksapp.respositories.BookJpaRepository;
+//import com.bna.booksapp.respositories.IBookRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class BookService implements IBookService{
-    IBookRepository bookRepository;
+    BookJpaRepository bookRepository;
+    BookMapper bookMapper;
 
-    public BookService(IBookRepository bookRepository) {
+    public BookService(BookJpaRepository bookRepository, BookMapper bookMapper) {
         this.bookRepository = bookRepository;
+        this.bookMapper = bookMapper;
     }
 
     @Override
     public List<BookDto> getBooks() {
-        return bookRepository.findAll();
+        List<Book> books = bookRepository.findAll();
+        return bookMapper.booksDtos(books);
+    }
+
+
+    @Override
+    public BookDto getBookById(long id) {
+        Book book = bookRepository.findById(id).orElse(null);
+        return bookMapper.toBookDto(book);
+    }
+
+
+    @Override
+    public BookDto addBook(BookDto bookDto) {
+        Book book = bookRepository.save(bookMapper.toBook(bookDto));
+        return bookMapper.toBookDto(book);
+    }
+
+
+    @Override
+    public BookDto updateBook(long id, BookDto bookDto) {
+        Book book = bookRepository.findById(id).orElse(null);
+        bookDto.setId(book.getId());
+        bookRepository.save(bookMapper.toBook(bookDto));
+        return bookMapper.toBookDto(book);
+    }
+
+    @Override
+    public BookDto deleteBook(long id) {
+        Book book = bookRepository.findById(id).orElse(null);
+        bookRepository.deleteById(id);
+        return bookMapper.toBookDto(book);
     }
 }
